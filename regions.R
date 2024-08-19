51945cf8d519b5864abf2eda7e98fa61892caaa5
sanempekel <sanempekel@gmail.com>
2024-08-19 12:00:29 +0300

    Update regions.R

--- regions.R
+++ regions.R
@@ -26,6 +26,24 @@ ggplot(df, aes(x = Region, y = Cost.of.Living.Index, fill = Region)) +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1),
         legend.position = "none")
 
+#filtering countries in the highest region
+europe_central_asia <- df %>%
+  filter(Region == "Europe & Central Asia")
 
+#sorting
+europe_central_asia <- europe_central_asia %>%
+  arrange(desc(Cost.of.Living.Index))
 
-                         
+europe_central_asia$Country <-
+  factor(europe_central_asia$Country, levels = europe_central_asia$Country)
+
+# visualization
+ggplot(europe_central_asia, aes(x = Country, y = Cost.of.Living.Index, fill = Region)) +
+  geom_bar(stat = "identity") +
+  labs(title = "Cost of Living Index by Country",
+       x = "Countries",
+       y = "Cost of Living Index") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1),
+        legend.position = "none")
+
