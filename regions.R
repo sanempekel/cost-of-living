2f93ec5d67c89da2e05326bc8f6a823b9a38c6bb
sanempekel <sanempekel@gmail.com>
2024-08-19 11:45:20 +0300

    Update regions.R

--- regions.R
+++ regions.R
@@ -15,17 +15,13 @@ df$Region <- countrycode(df$Country, "country.name", "region")
 df <- df %>%
   select(Rank, Country, Region, everything())
 
-# average cost of living by region
-regions <- split(df, df$Region)
-cost_of_living_by_regions <- 
-  aggregate(Cost.of.Living.Index ~ Region, data = df, FUN = mean)
 
 # visualization
-ggplot(cost_of_living_by_regions, aes(x = Region, y = Cost.of.Living.Index, fill = Region)) +
+ggplot(df, aes(x = Region, y = Cost.of.Living.Index, fill = Region)) +
   geom_bar(stat = "identity") +
-  labs(title = "Average Cost of Living Index by Region",
+  labs(title = "Cost of Living Index by Region",
        x = "Region",
-       y = "Average Cost of Living Index") +
+       y = "Cost of Living Index") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1),
         legend.position = "none")
